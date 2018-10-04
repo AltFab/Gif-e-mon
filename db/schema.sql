7ae4ea722d8402920000069bb838bec4a9dec5d0
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-10-04 10:36:28 -0700

    working on orm

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS gifemon_db;
+CREATE DATABASE gifemon_db;
+
+USE gifemon_db;
+
+CREATE TABLE gifedex(
+	id INT AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
+	score INT,
+    gif_name VARCHAR(200) NULL,
+    gif_link VARCHAR (max) NOT NULL,
+    health INT NOT NULL,
+    attack INT NOT NULL,
+    potion INT(3) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+
+CREATE TABLE gifbattles(
+	id INT AUTO_INCREMENT NOT NULL,
+    enemy_gif_name VARCHAR(200) NOT NULL,
+    enemy_gif_link VARCHAR(max) NOT NULL,
+    enemy_attack INT NOT NULL,
+    enemy_hp INT NOT NULL,
+    enemy_potion INT(1) NOT NULL,
+	PRIMARY KEY(id)
+);
--- game_play/attack.js
+++ game_play/attack.js
@@ -1,8 +1,38 @@
-function attack(){
+switch (expression) {
+    case attack:
+        attack();
+        break;
+    case defend:
+        defend();
+        break;
+    case potion:
+       potion();
+        break;
+}
+
+
+
+
+function attack() {
     //animate icon
     //decrease hp
-        // check if Enemy Hp = zero
-            //win round function
-            //next round
-    //end turn function called
+    // check if Enemy Hp = zero
+    if (ENEMY.HP === 0) {
+        //userscore increase by 20 points
+        winBonus = 20;
+        user.score = user.score + winBonus;
+
+        //update those to database
+
+        nextRound();
+    } else {
+        ENEMY.HP = ENEMY.HP - user.attackpower
+
+        //update new hp to database
+
+        endTurn()
+    }
+
+
+
 }
