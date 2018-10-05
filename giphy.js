f23b9ac9b56332e664dcc9253270d1af4065c16f
Dillon Reinwald <dreinwaldmail@gmail.com>
2018-10-05 10:23:10 -0700

    disabled buttons for builds once clicked. alted stats. need to do mysql talking

--- giphy.js
+++ giphy.js
@@ -1,44 +1,67 @@
 // initialize jquery
-$(document).ready(function(){
+$(document).ready(function () {
     var randoGif = {
-        gif_link:"", 
-        user_name:"",
-        health:10,
-        attack:0,
-        potion:3,
+        gif_link: "",
+        user_name: "",
+        health: 10,
+        attack: 0,
+        potion: 3,
     };
+    console.log("base gif stats")
     console.log(randoGif);
 
-// on click function generates and pulls gif from giphy
-$("button#random-gif.btn.btn-primary").on("click", function(){
-// this method rotates to the next gif and removes the current one
-    $("#gifs").empty();
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13&tag=cartoon";
+    // on click function generates and pulls gif from giphy
+    $("button#random-gif.btn.btn-primary").on("click", function () {
+        // this method rotates to the next gif and removes the current one
+        $("#gifs").empty();
+        var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&rating=pg-13&tag=cartoon";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function(response){
-        
-        var results = response.data;
-        console.log(response);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
 
-        var image = $("<img>");
+                var results = response.data;
 
-        image.attr("src", results.images.fixed_height.url);
-        image.attr("class", "item");
 
-        randoGif.gif_link = results.images.fixed_height.url;
-// populates the gif
-        $("#gifs").prepend(image);
-        });
+                var image = $("<img>");
+
+                image.attr("src", results.images.fixed_height.url);
+                image.attr("class", "item");
+
+                randoGif.gif_link = results.images.fixed_height.url;
+                // populates the gif
+                $("#gifs").prepend(image);
+            });
     });
+    $(".buildButton").on("click", function () {
+        var buildChosen = $(this).val()
+        console.log(buildChosen)
+        console.log(typeof(buildChosen))
+        switch (buildChosen) {
+            case "attack":{
+                randoGif.attack = 6;
+                randoGif.health = randoGif.health + 4;}
+                break;
+            case "defend":{
+                randoGif.attack = 4;
+                randoGif.health = randoGif.health + 6;}
+                break;
+            case "balance":{
+                randoGif.attack = 5;
+                randoGif.health = randoGif.health + 5;}
+                break;
+        }
 
-$("#start").on("click", function(){
-    randoGif.user_name = $("#username").val().trim();
-    //send to mysql  randoGif
-    
-    console.log(randoGif);
-});
+        $(".buildButton").attr("disabled", "disabled");
+
+    })
+
+    $("#start").on("click", function () {
+        randoGif.user_name = $("#username").val().trim();
+        //send to mysql  randoGif
+        console.log("gif stats modified")
+        console.log(randoGif);
+    });
 });
